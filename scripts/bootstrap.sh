bec96d1b99d247764f0c6640a2d982e71b08c410
Chaminda Divitotawela <cdivitotawela@gmail.com>
2020-12-21 13:24:39 +1000

    test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,11 @@
 REPO_HOME='..'
 
 # Install Flux
-for manifest in $(ls $REPO_HOME/helm/flux/*.yaml)
+for manifest in $(ls $REPO_HOME/manifests/flux/*.yaml)
 do
   kubectl apply -f $manifest
 done
+
+helm upgrade -i flux fluxcd/flux \
+     --namespace flux \
+     -f $REPO_HOME/helm/flux/values.yml
